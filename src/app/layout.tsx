c29268cd2703c5a8bf33d01d1a9f79515b3fdfc9
youngduck <dudejr5839@naver.com>
2024-03-06 17:03:52 +0900

    sitemap 동적생성, 메인페이지 구글seo추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "김영덕 기술블로그",
   description: "개발자 김영덕의 기술블로그입니다.",
+  authors: { name: "youngduck" },
+  generator: "Next.js",
+  creator: "youngduck",
+  publisher: "Vercel",
+  verification: { google: "lybtoBCBf6isHIGdGbYxTHG7N2dUanHjWahiXMgKtXY" },
   icons: {
     icon: "/favicon.ico",
   },
---
+++ src/app/sitemap.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from "next";
+import { getAllPosts } from "@/lib/api";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const results: MetadataRoute.Sitemap = [];
+  const posts = getAllPosts();
+  const baseUrl = process.env.NEXT_PUBLIC_BASED_URL;
+
+  for (let i = 0; i < posts.length; i++) {
+    results.push({
+      url: `${baseUrl}/posts/${posts[i].slug}`,
+      lastModified: new Date(),
+    });
+  }
+
+  return results;
+}
