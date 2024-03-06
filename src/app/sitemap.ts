f8a80d409d1bc65811368a4ac9930b9a7558dcbb
youngduck <dudejr5839@naver.com>
2024-03-06 17:07:20 +0900

    사이트맵 메인페이지누락 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,6 +6,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const posts = getAllPosts();
   const baseUrl = process.env.NEXT_PUBLIC_BASED_URL;
 
+  results.push({
+    url: `${baseUrl}`,
+    lastModified: new Date(),
+  });
+
   for (let i = 0; i < posts.length; i++) {
     results.push({
       url: `${baseUrl}/posts/${posts[i].slug}`,
