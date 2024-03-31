e11ad84798c089f17773883cc3ed4e6182b3ec71
youngduck <dudejr5839@naver.com>
2024-03-31 23:09:07 +0900

    design: footer 컨텐츠 변경

--- src/app/_components/layout/footer.tsx
+++ src/app/_components/layout/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full px-6 py-6 text-center ">
       <div className="space-y-4 xl:space-y-6">
-        <div className="relative mx-auto w-36 h-36 rounded-full border-2">
+        <div className="relative mx-auto w-36 h-36 rounded-full border-2 border-yellow">
           <Image
             src="/assets/blog/authors/youngduck.png"
             alt="/assets/blog/authors/youngduck.png"
@@ -14,33 +14,13 @@ const Footer = () => {
             className="object-cover rounded-full"
           />
         </div>
-        <div className="space-y-2">
-          <div className="flex justify-center items-center flex-col space-y-3 text-lg font-medium leading-6">
-            <span className="text-yellow-500">김영덕</span>
-            <p className="text-yellow-500">Frontend Developer</p>
+        <div className="space-y-2 text-yellow">
+          <div className="flex justify-center items-center flex-col space-y-3 text-lg leading-6">
+            <p className="text-[20px]">김영덕</p>
+            <p className="text-[14px]">Frontend Developer</p>
             <div className="flex justify-center mt-5 space-x-5">
               <a
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-block text-gray-400"
-              >
-                <span className="sr-only">Twitter</span>
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 512 512"
-                  className="w-6 h-6 text-gray-400 hover:text-gray-100"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
-                </svg>
-              </a>
-              <a
-                href="#"
+                href="https://github.com/youngduck"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block text-gray-400"
@@ -51,7 +31,7 @@ const Footer = () => {
                   fill="currentColor"
                   strokeWidth="0"
                   viewBox="0 0 496 512"
-                  className="w-6 h-6 text-gray-400 hover:text-gray-100"
+                  className="w-6 h-6 text-gray-400 hover:text-yellow"
                   height="1em"
                   width="1em"
                   xmlns="http://www.w3.org/2000/svg"
@@ -60,23 +40,46 @@ const Footer = () => {
                 </svg>
               </a>
               <a
-                href="#"
+                href="https://dev-youngduck.notion.site/ec16c2874ca74ea5b023b8e7ef2384e5?pvs=4"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block text-gray-400"
               >
-                <span className="sr-only">Linkedin</span>
+                <span className="sr-only">Notion</span>
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 15 15"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  strokeWidth="0"
+                  className="w-6 h-6 text-gray-400 hover:text-yellow"
+                >
+                  <path d="M3.25781 3.11684C3.67771 3.45796 3.83523 3.43193 4.62369 3.37933L12.0571 2.93299C12.2147 2.93299 12.0836 2.77571 12.0311 2.74957L10.7965 1.85711C10.56 1.67347 10.2448 1.46315 9.64083 1.51576L2.44308 2.04074C2.18059 2.06677 2.12815 2.19801 2.2327 2.30322L3.25781 3.11684ZM3.7041 4.84917V12.6704C3.7041 13.0907 3.91415 13.248 4.38693 13.222L12.5562 12.7493C13.0292 12.7233 13.0819 12.4341 13.0819 12.0927V4.32397C13.0819 3.98306 12.9508 3.79921 12.6612 3.82545L4.12422 4.32397C3.80918 4.35044 3.7041 4.50803 3.7041 4.84917ZM11.7688 5.26872C11.8212 5.50518 11.7688 5.74142 11.5319 5.76799L11.1383 5.84641V11.6205C10.7965 11.8042 10.4814 11.9092 10.2188 11.9092C9.79835 11.9092 9.69305 11.7779 9.37812 11.3844L6.80345 7.34249V11.2532L7.61816 11.437C7.61816 11.437 7.61816 11.9092 6.96086 11.9092L5.14879 12.0143C5.09615 11.9092 5.14879 11.647 5.33259 11.5944L5.80546 11.4634V6.29276L5.1489 6.24015C5.09625 6.00369 5.22739 5.66278 5.5954 5.63631L7.53935 5.50528L10.2188 9.5998V5.97765L9.53564 5.89924C9.4832 5.61018 9.69305 5.40028 9.95576 5.37425L11.7688 5.26872ZM1.83874 1.33212L9.32557 0.780787C10.245 0.701932 10.4815 0.754753 11.0594 1.17452L13.4492 2.85424C13.8436 3.14309 13.975 3.22173 13.975 3.53661V12.7493C13.975 13.3266 13.7647 13.6681 13.0293 13.7203L4.33492 14.2454C3.78291 14.2717 3.52019 14.193 3.23111 13.8253L1.47116 11.5419C1.1558 11.1216 1.02466 10.8071 1.02466 10.4392V2.25041C1.02466 1.77825 1.23504 1.38441 1.83874 1.33212Z" />
+                </svg>
+              </a>
+              <a
+                href="https://velog.io/@youngduck/posts"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block text-gray-400"
+              >
+                <span className="sr-only">Velog</span>
+
                 <svg
                   stroke="currentColor"
                   fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 448 512"
-                  className="w-6 h-6 text-gray-400 hover:text-gray-100"
-                  height="1em"
-                  width="1em"
+                  className="w-6 h-6 text-gray-400 hover:text-yellow"
                   xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 192 192"
+                  enableBackground="new 0 0 28 28"
+                  width="1em"
+                  height="1em"
                 >
-                  <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+                  <path
+                    id="Website"
+                    d="M24 0H168C181.255 0 192 10.7451 192 24V168C192 181.255 181.255 192 168 192H24C10.7451 192 0 181.255 0 168V24C0 10.7451 10.7451 0 24 0ZM49 57.9199V65.48H67L80.6799 142.52L98.5 141.26C116.02 119.06 127.84 102.44 133.96 91.3999C140.2 80.24 143.32 70.9399 143.32 63.5C143.32 59.0601 142 55.7 139.36 53.4199C136.84 51.1399 133.66 50 129.82 50C122.62 50 116.62 53.0601 111.82 59.1799C116.5 62.3 119.68 64.8799 121.36 66.9199C123.16 68.8401 124.06 71.4199 124.06 74.6599C124.06 80.0601 122.44 86.1799 119.2 93.02C116.08 99.8601 112.66 105.92 108.94 111.2C106.54 114.56 103.48 118.7 99.76 123.62L88.0601 57.2C87.1001 52.3999 84.1001 50 79.0601 50C76.78 50 72.3999 50.96 65.9199 52.8799C59.4399 54.6799 53.8 56.3601 49 57.9199Z"
+                  />
                 </svg>
               </a>
             </div>
