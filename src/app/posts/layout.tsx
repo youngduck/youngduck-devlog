48576f1cc931ad8a9678ab74d3a0c71efbad5e50
youngduck <dudejr5839@naver.com>
2024-06-04 10:27:45 +0900

    feat: 게시판 최신글보기 추가

---
+++ src/app/posts/layout.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PostCard from "../_components/post/post-card";
+import { getAllPosts } from "@/lib/api";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  const posts = getAllPosts();
+
+  return (
+    <div className="lg:w-[1150px] mx-auto">
+      {children}
+      <div className="lg:w-[960px] border-y-2 py-6 px-4 text-[24px] font-[KCC]">
+        Posts
+      </div>
+      <div className="flex lg:w-[960px] flex-wrap lg:justify-normal justify-center border-b-2 py-6">
+        {posts.slice(0, 3).map((item, idx) => (
+          <PostCard
+            key={idx}
+            slug={item.slug}
+            title={item.title}
+            coverImage={item.coverImage}
+            date={item.date}
+            excerpt={item.excerpt}
+            category={item.category}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default layout;
