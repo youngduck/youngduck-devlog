462a0a11204db0a85fefac6e91a2cd1380ee60c3
youngduck <dudejr5839@naver.com>
2024-12-19 11:42:06 +0900

    init: storybook 세팅

---
+++ .storybook/manager.ts
@@ -0,0 +1,16 @@
+import { addons } from "@storybook/manager-api";
+import { create } from "@storybook/theming/create";
+
+const theme = create({
+  base: "light", // 또는 'dark'
+
+  // 브랜드
+  brandTitle: "YD-DEVLOG",
+  brandUrl: "https://youngduck-devlog.vercel.app/",
+  brandImage: "/assets/logo/logo.png",
+  brandTarget: "_blank",
+});
+
+addons.setConfig({
+  theme: theme,
+});
