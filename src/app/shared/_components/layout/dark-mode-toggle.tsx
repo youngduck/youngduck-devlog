089a86bd91239986389df44e7f73a621bec24711
youngduck <dudejr5839@gmail.com>
2025-02-04 00:42:39 +0900

    refactor: 절대경로로변경

--- src/app/shared/_components/layout/dark-mode-toggle.tsx
+++ src/app/shared/_components/layout/dark-mode-toggle.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { Button } from "../../../../components/ui/button";
+import { Button } from "@/components/ui/button";
 
 export function DarkModeToggle() {
   const { resolvedTheme, setTheme } = useTheme();
--- src/app/shared/_components/layout/rss-feed-button/rss-feed-button.tsx
+++ src/app/shared/_components/layout/rss-feed-button/rss-feed-button.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../../../../../components/ui/button";
+import { Button } from "@/components/ui/button";
 import RssFeedIcon from "@public/assets/svg/rssFeedIcon.svg";
 
 const RssFeedButton = () => {
