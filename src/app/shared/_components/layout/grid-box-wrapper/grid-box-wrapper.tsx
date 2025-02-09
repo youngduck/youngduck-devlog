56b9b907cf04798af2bc3e973ceb73939b5de329
youngduck <dudejr5839@gmail.com>
2025-02-09 20:51:49 +0900

    feat: scrap-api 이상현상 수정

--- src/app/shared/_components/layout/grid-box-wrapper/grid-box-wrapper.tsx
+++ src/app/shared/_components/layout/grid-box-wrapper/grid-box-wrapper.tsx
@@ -1,31 +1,51 @@
-import React from "react";
+/**
+ * 작성자: KYD
+ * 기능:  1. 그리드 박스 레이아웃에 쓰일 레이아웃 컴포넌트 (헤더+컨텐츠)
+ * 기능:  2. title,option 존재시 헤더 레이아웃 존재
+ * 기능:  3. 컨텐츠 레이아웃 존재
+ * 프로세스 설명: 프로세스 복잡시 노션링크 첨부권장
+ * 아이디어: Tailwind overiding 적용
+ * TODO: TW-MACRO 전환
+ */
 
 interface GridBoxWrapperProps {
   children: React.ReactNode;
   className: string;
   title?: string;
+  option?: React.ReactNode;
 }
 
 const GridBoxWrapper: React.FC<GridBoxWrapperProps> = ({
   children,
   className,
   title,
+  option,
 }) => {
+  const titleHeight = title ? "24px" : "0px";
   return (
     <section
       className={`h-full w-full transform animate-fade-up rounded-md border-2 bg-secondary p-2 ${className}`}
     >
-      <div className="flex justify-between">
-        <div>{title}</div>
-        <div></div>
+      {title && (
+        <div
+          className={`flex w-full items-center justify-between`}
+          style={{
+            height: titleHeight,
+          }}
+        >
+          <div className="text-md">{title}</div>
+          <div>{option}</div>
+        </div>
+      )}
+      <div
+        className={`w-full overflow-y-auto`}
+        style={{
+          height: `calc(100% - ${titleHeight})`,
+        }}
+      >
+        {children}
       </div>
-      {children}
     </section>
-    // <div
-    //   className={`rounded-[6px] bg-gradient-to-r from-yellow to-[#8C6306] p-1 w-full ${className}`}
-    // >
-    //   <div className="flex h-full w-full p-2 bg-secondary">{children}</div>
-    // </div>
   );
 };
 
