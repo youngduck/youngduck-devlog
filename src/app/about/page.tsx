238248b613904187c8bae9af4d1f377f4189cfd1
youngduck <dudejr5839@gmail.com>
2025-03-04 23:27:12 +0900

    feat: about페이지 초안

---
+++ src/app/about/page.tsx
@@ -0,0 +1,37 @@
+/**
+ * 작성자: KYD
+ * 기능: 소개 페이지 서버사이드 렌더링
+ * 프로세스 설명: 프로세스 복잡시 노션링크 첨부권장
+ */
+import React from "react";
+
+interface Ipage {}
+
+const page: React.FC<Ipage> = () => {
+  //SECTION HOOK호출 영역
+
+  //!SECTION HOOK호출 영역
+
+  //SECTION 상태값 영역
+
+  //!SECTION 상태값 영역
+
+  //SECTION 메서드 영역
+
+  //!SECTION 메서드 영역
+
+  return (
+    <main className="mx-auto h-auto w-full lg:w-[1200px]">
+      <article className="flex flex-col gap-4">
+        <h1>About</h1>
+        <p>
+          <strong>
+            <span>dd</span>
+          </strong>
+        </p>
+      </article>
+    </main>
+  );
+};
+
+export default page;
