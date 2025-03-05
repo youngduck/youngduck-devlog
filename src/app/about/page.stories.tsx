d1241a141b151b0df1c438587356ad9949164fe1
youngduck <46455370+youngduck@users.noreply.github.com>
2025-03-05 23:23:40 +0900

    Yd 5 feat about페이지 임시 제작 (#27)
    * feat: about페이지 초안
    
    * feat: 어바웃페이지 제작중 공지

---
+++ src/app/about/page.stories.tsx
@@ -0,0 +1,55 @@
+import { Meta, StoryObj } from "@storybook/react";
+import About from "./page";
+import { ThemeProvider } from "@/app/shared/_components/provider/theme-provider";
+import Header from "@layout/header/header";
+import Footer from "@/app/shared/_components/layout/footer";
+
+const meta: Meta<typeof About> = {
+  title: "pages/About",
+  component: About,
+  decorators: [
+    (Story) => (
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Header />
+        <Story />
+        <Footer />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof About>;
+
+export const Desktop: Story = {
+  parameters: {
+    layout: "fullscreen",
+    viewport: {
+      defaultViewport: "desktop",
+    },
+  },
+};
+
+export const Ipad11Pro: Story = {
+  parameters: {
+    layout: "fullscreen",
+    viewport: {
+      defaultViewport: "ipad11p",
+    },
+  },
+};
+
+export const Mobile: Story = {
+  parameters: {
+    layout: "fullscreen",
+    viewport: {
+      defaultViewport: "iphone6",
+    },
+  },
+};
