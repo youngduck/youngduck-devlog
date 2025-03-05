098529319c9d81fdedb15fe9876ad84bdbf13b60
youngduck <dudejr5839@gmail.com>
2025-03-05 23:21:14 +0900

    feat: 어바웃페이지 제작중 공지

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,11 +4,13 @@
  * 프로세스 설명: 프로세스 복잡시 노션링크 첨부권장
  */
 import React from "react";
-
+import PostBody from "@/app/shared/_components/post/post-body/post-body";
+import GridBoxWrapper from "@layout/grid-box-wrapper/grid-box-wrapper";
 interface Ipage {}
 
 const page: React.FC<Ipage> = () => {
   //SECTION HOOK호출 영역
+  const mdContent = "> About페이지는 현재 제작중입니다.";
 
   //!SECTION HOOK호출 영역
 
@@ -21,15 +23,15 @@ const page: React.FC<Ipage> = () => {
   //!SECTION 메서드 영역
 
   return (
-    <main className="mx-auto h-auto w-full lg:w-[1200px]">
-      <article className="flex flex-col gap-4">
-        <h1>About</h1>
-        <p>
-          <strong>
-            <span>dd</span>
-          </strong>
-        </p>
-      </article>
+    <main className="flex h-auto w-full lg:mx-auto lg:w-[1200px]">
+      <div className="grid w-full grid-cols-1 grid-rows-1 gap-[20px] lg:h-[480px] lg:grid-cols-[250px_600px_310px] lg:grid-rows-[110px_350px]">
+        <GridBoxWrapper className="rounded-t-none border-t-0 bg-contain lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3">
+          목차, 인쇄
+        </GridBoxWrapper>
+        <GridBoxWrapper className="border-0 lg:col-start-2 lg:col-end-4 lg:row-start-1 lg:row-end-3">
+          <PostBody content={mdContent} />
+        </GridBoxWrapper>
+      </div>
     </main>
   );
 };
