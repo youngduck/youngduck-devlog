8d65c6a65b6cff4353657d9e0b9f4223c7d8889d
youngduck <dudejr5839@naver.com>
2025-03-13 15:46:26 +0900

    fix: sitemap.xml 생성

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,11 @@
 import { MetadataRoute } from "next";
 import { getAllPosts } from "@/lib/api";
+import { getAllAlgorithms } from "@/app/api/algorithms/api";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const results: MetadataRoute.Sitemap = [];
   const posts = getAllPosts();
+  const algorithms = getAllAlgorithms();
   const baseUrl = process.env.NEXT_PUBLIC_BASED_URL;
 
   results.push({
@@ -13,8 +15,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   for (let i = 0; i < posts.length; i++) {
     results.push({
-      url: `${baseUrl}/posts/${posts[i].slug}`,
-      lastModified: new Date(posts[i].date.replace("/", ",")),
+      url: `${baseUrl}/blog/posts/${posts[i].slug}`,
+      lastModified: new Date(posts[i].date),
+    });
+  }
+
+  for (let i = 0; i < algorithms.length; i++) {
+    results.push({
+      url: `${baseUrl}/algorithms/ps/${algorithms[i].slug}`,
+      lastModified: new Date(algorithms[i].date),
     });
   }
 
