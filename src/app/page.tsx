c7b9136bd35d0641604cf6803f6648c1e5f4d78d
youngduck <dudejr5839@naver.com>
2025-03-18 00:40:20 +0900

    feat: 반응형 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,25 @@ import ScrapItem from "./home/components/scrap/scrap-item/scrap-item";
 import ChartsFunnel from "./domains/charts/charts-funnel/ChartsFunnel";
 
 import Profile from "./domains/profile/profile";
+import { getAllPosts } from "@/lib/api";
+import PostCards from "./shared/_components/post/post-cards/post-cards";
+import Link from "next/link";
+import { getAllAlgorithms } from "./api/algorithms/api";
 export default async function Home() {
+  const posts = getAllPosts();
+  const algorithms = getAllAlgorithms();
+
   return (
     <main className="mx-auto h-auto w-full transform animate-fade-up duration-500 md:w-[870px] lg:w-[1200px]">
       {/* 첫 번째 행 */}
       <div className="grid w-full gap-[20px] sm:grid-cols-1 sm:grid-rows-1 md:grid-cols-[250px_600px] md:grid-rows-[110px_350px_350px] lg:grid-cols-[250px_600px_310px] lg:grid-rows-[110px_350px]">
-        <GridBoxWrapper className="rounded-t-none border-t-0 bg-contain md:col-[1/2] md:row-[1/3] lg:col-[1/2] lg:row-[1/3]">
+        <GridBoxWrapper className="rounded-t-none border-t-0 bg-contain sm:h-[350px] md:col-[1/2] md:row-[1/3] md:h-auto lg:col-[1/2] lg:row-[1/3] lg:h-auto">
           <Profile />
         </GridBoxWrapper>
         <GridBoxWrapper className="border-transparent bg-transparent md:col-[2/3] md:row-[1/2] lg:col-[2/3] lg:row-[1/2]">
-          <div className="text-[18px]">
-            {/* <p>안녕하세요!</p>
-            <p>시각화를 좋아하는 개발자 김영덕입니다.</p>
+          <div className="text-xl italic">
+            <p>안녕하세요!</p>
+            {/* <p>시각화를 좋아하는 개발자 김영덕입니다.</p>
             <p>
               기록하며 성장하는 개발자가 되기위해 기술블로그를 운영중입니다.
             </p> */}
@@ -25,10 +32,44 @@ export default async function Home() {
         </GridBoxWrapper>
         <GridBoxWrapper
           title="최근 스크랩한 게시글"
-          className="border-2 sm:h-[250px] md:col-[1/3] md:row-[3/4] md:h-auto lg:col-[3/4] lg:row-[2/3] lg:h-auto"
+          className="border-2 sm:h-[350px] md:col-[1/3] md:row-[3/4] md:h-auto lg:col-[3/4] lg:row-[2/3] lg:h-auto"
         >
           <ScrapItem />
         </GridBoxWrapper>
+      </div>
+      <div className="mx-auto h-auto w-full md:w-[870px] lg:w-[1200px]">
+        <div className="flex flex-col items-center justify-center">
+          <div className="my-[20px] block w-full transform animate-fade-up border-y-[3px] p-3 font-[KCC] duration-500 md:w-[870px] lg:w-[1200px]">
+            <ul className="flex flex-wrap justify-between">
+              <li className="mx-2 font-bold italic pcHover:hover:scale-105">
+                <span className="text-xl">최근 Blog</span>
+              </li>
+              <li className="mx-2 font-bold italic pcHover:hover:scale-105">
+                <Link href="/blog">
+                  <span className="text-xl">더보기</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <PostCards posts={posts.slice(0, 4)} domain="blog" />
+        </div>
+      </div>
+      <div className="mx-auto h-auto w-full md:w-[870px] lg:w-[1200px]">
+        <div className="flex flex-col items-center justify-center">
+          <div className="my-[20px] block w-full transform animate-fade-up border-y-[3px] p-3 font-[KCC] duration-500 md:w-[870px] lg:w-[1200px]">
+            <ul className="flex flex-wrap justify-between">
+              <li className="mx-2 font-bold italic pcHover:hover:scale-105">
+                <span className="text-xl">최근 알고리즘</span>
+              </li>
+              <li className="mx-2 font-bold italic pcHover:hover:scale-105">
+                <Link href="/algorithms">
+                  <span className="text-xl">더보기</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <PostCards posts={algorithms.slice(0, 4)} domain="algorithms" />
+        </div>
       </div>
     </main>
   );
--- src/app/shared/_components/layout/tag-navbar.tsx
+++ src/app/shared/_components/layout/tag-navbar.tsx
@@ -5,7 +5,7 @@ const TagNavbar = async () => {
   const categories = await getAllCategoriesArray();
 
   return (
-    <nav className="my-[30px] block w-full transform animate-fade-up border-y-2 p-3 font-[KCC] duration-500 lg:w-[1200px]">
+    <nav className="my-[30px] block w-full transform animate-fade-up border-y-2 p-3 font-[KCC] duration-500 md:w-[870px] lg:w-[1200px]">
       <ul className="flex flex-wrap">
         {categories.map((item: [string, number], idx: number) => (
           <Link
