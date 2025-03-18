a3c1d605dac8eb123266dae126970de0fec4f09c
youngduck <dudejr5839@naver.com>
2025-03-18 22:31:12 +0900

    refactor: 가독성을 높이기위해 상단부에서 선언

--- src/app/algorithms/components/tag-navbar/tag-navbar.tsx
+++ src/app/algorithms/components/tag-navbar/tag-navbar.tsx
@@ -4,18 +4,17 @@ import { getAllCategoriesArray } from "@/app/api/algorithms/api";
 const TagNavbar = async () => {
   const categories = await getAllCategoriesArray();
 
+  const getNavigateUrl = (category: string) => {
+    return category === "All Algorithms"
+      ? "/algorithms"
+      : `/algorithms/${category}`;
+  };
+
   return (
     <nav className="my-[30px] block w-full transform animate-fade-up border-y-2 p-3 font-[KCC] duration-500 md:w-[870px] lg:w-[1200px]">
       <ul className="flex flex-wrap">
         {categories.map((item: [string, number], idx: number) => (
-          <Link
-            key={idx}
-            href={
-              item[0] === "All Algorithms"
-                ? "/algorithms"
-                : `/algorithms/${item[0]}`
-            }
-          >
+          <Link key={idx} href={getNavigateUrl(item[0])}>
             <li className="mx-2 font-bold italic pcHover:hover:scale-105">
               <span className="text-xl"># {item[0]}</span>
               <span className="text-xs">({item[1]})</span>
--- src/app/home/components/tag-navbar/tag-navbar.tsx
+++ src/app/home/components/tag-navbar/tag-navbar.tsx
@@ -4,14 +4,15 @@ import { getAllCategoriesArray } from "@/lib/api";
 const TagNavbar = async () => {
   const categories = await getAllCategoriesArray();
 
+  const getNavigateUrl = (category: string) => {
+    return category === "All Posts" ? "/blog" : `/blog/${category}`;
+  };
+
   return (
     <nav className="my-[30px] block w-full transform animate-fade-up border-y-2 p-3 font-[KCC] duration-500 md:w-[870px] lg:w-[1200px]">
       <ul className="flex flex-wrap">
         {categories.map((item: [string, number], idx: number) => (
-          <Link
-            key={idx}
-            href={item[0] === "All Posts" ? "/" : `/blog/${item[0]}`}
-          >
+          <Link key={idx} href={getNavigateUrl(item[0])}>
             <li className="mx-2 font-bold italic pcHover:hover:scale-105">
               <span className="text-xl"># {item[0]}</span>
               <span className="text-xs">({item[1]})</span>
