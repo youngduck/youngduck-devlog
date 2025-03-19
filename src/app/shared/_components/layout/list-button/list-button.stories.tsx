fc92ca69055cb4bc4877b6eed46adbff46b01ce2
youngduck <dudejr5839@naver.com>
2025-03-19 09:07:10 +0900

    hotFix: listButton 컴포넌트 빌드 오류 수정

--- src/app/shared/_components/layout/list-button/list-button.stories.tsx
+++ src/app/shared/_components/layout/list-button/list-button.stories.tsx
@@ -1,18 +1,26 @@
-import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
 import ListButton from "./list-button";
 
 const meta: Meta<typeof ListButton> = {
   title: "Components/Layout/ListButton",
   component: ListButton,
-  argTypes: {},
-  tags: ["autodocs"],
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof ListButton>;
 
-export const Default: Story = {
-  render: () => <ListButton />,
+export const Mobile: Story = {
+  args: {
+    onClick: () => console.log("Button clicked"),
+  },
+  parameters: {
+    layout: "fullscreen",
+    viewport: {
+      defaultViewport: "iphone6",
+    },
+  },
 };
