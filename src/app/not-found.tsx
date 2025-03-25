d01b5b3d4f160f49f87c69c1266d17e40727e4ab
youngduck <dudejr5839@gmail.com>
2025-03-25 21:57:33 +0900

    feat: 404페이지 다이나믹임포트 제거

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,9 @@
-import dynamic from "next/dynamic";
 import { lottieData } from "@/app/shared/_components/lottie/lottieData";
-
-const CustomLottie = dynamic(
-  () => import("@/app/shared/_components/lottie/CustomLottie"),
-  { ssr: false },
-);
+import CustomLottie from "@/app/shared/_components/lottie/CustomLottie";
 
 const NotFound = () => {
   return (
-    <main className="md:max-w-container-md lg:max-w-container-lg mx-auto h-auto">
+    <main className="mx-auto h-auto md:max-w-container-md lg:max-w-container-lg">
       <div className="flex flex-col items-center justify-center border-b-2">
         <h1 className="font-[KCC] text-[40px] text-purple-500">
           존재 하지 않는 페이지 입니다.
