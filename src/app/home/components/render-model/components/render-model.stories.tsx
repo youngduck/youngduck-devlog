5cf1b8a56e5cfef3f077e31c951890703a070231
youngduck <dudejr5839@gmail.com>
2025-04-03 23:09:21 +0900

    del: threejs컴포넌트제거

--- src/app/home/components/render-model/components/render-model.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from "@storybook/react";
-import RenderModel from "./render-model";
-
-export default {
-  title: "Domains/RenderModel",
-  component: RenderModel,
-} as Meta<typeof RenderModel>;
-
-export const Default: StoryObj<typeof RenderModel> = {};
