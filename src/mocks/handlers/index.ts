1a32de4282954e7c96e4505a481989a53ff683dd
youngduck <dudejr5839@naver.com>
2025-04-03 17:53:56 +0900

    docs: route hander전환에따른 msw 목데이터 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,3 +1,9 @@
-import { scrapHandlers } from "./scrap";
+import { scrapHandlers } from "./scrap-item/scrap";
+import { blogHandlers } from "./blog/blogHandler";
+import { algorithmsHandlers } from "./algorithms/algorithmsHandler";
 
-export const handler = [...scrapHandlers];
+export const handler = [
+  ...scrapHandlers,
+  ...blogHandlers,
+  ...algorithmsHandlers,
+];
